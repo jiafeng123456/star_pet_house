fc9c7a755502e238434831341b6fda06e6276771
jiafeng123456 <1020072926@qq.com>
2020-11-02 15:55:30 +0800

    1

---
+++ star_pet_house_provider/src/main/java/com/jf/provider/aop/ProviderAsecpt.java
@@ -0,0 +1,26 @@
+package com.jf.provider.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/*
+ *@description:
+ *@author jiafeng
+ *@date 2020/9/27 0027 10:47
+ */
+@Aspect
+@Component
+public class ProviderAsecpt {
+
+    @Pointcut("execution(public * com.jf.provider.impl..*(..))")
+    public void BrokerAspect() {
+
+    }
+
+    @Before("BrokerAspect()")
+    public void doBefore() {
+        System.out.println("方法开始了呀，大家系好安全带！");
+    }
+}
