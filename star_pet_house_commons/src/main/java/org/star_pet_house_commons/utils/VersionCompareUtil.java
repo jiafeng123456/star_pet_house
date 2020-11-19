288987a41511ec94f77cf0608adba424548253f0
jiafeng123456 <1020072926@qq.com>
2020-11-19 10:42:05 +0800

    修改内容：增加V1.0.0类似版本号比较工具类

---
+++ star_pet_house_commons/src/main/java/org/star_pet_house_commons/utils/VersionCompareUtil.java
@@ -0,0 +1,33 @@
+package org.star_pet_house_commons.utils;
+
+/*
+ *@description: 版本号比较 版本号类似 V1.0.0
+ *@author jiafeng
+ *@date 2020/11/19 0019 10:17
+ */
+public class VersionCompareUtil {
+
+    public static void main(String[] args) {
+        String lastVersion = "V1.0.1";
+        String  version= "V1.0.2";
+        if (compareVersion(lastVersion, version)){
+            System.out.println("最新版本");
+        }else {
+
+        }
+    }
+
+
+    public static boolean compareVersion(String oldVersion, String newVersion){
+        String[] old_version_strs = oldVersion.replace("V", "").trim().split("\\.");
+        String[] new_version_strs = newVersion.replace("V", "").trim().split("\\.");
+        boolean is_new_version = false;
+        for (int i = 0 ; i < old_version_strs.length; i++){
+            if (Integer.valueOf(old_version_strs[i]) < Integer.valueOf(new_version_strs[i])){
+                is_new_version = true;
+                break;
+            }
+        }
+        return is_new_version;
+    }
+}
