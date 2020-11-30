016490f962a4a56f8d45354ca4787838a49494b9
jiafeng123456 <1020072926@qq.com>
2020-11-30 12:21:01 +0800

    修改内容：

--- star_pet_house_commons/src/main/java/org/star_pet_house_commons/utils/VersionCompareUtil.java
+++ star_pet_house_commons/src/main/java/org/star_pet_house_commons/utils/VersionCompareUtil.java
@@ -8,12 +8,12 @@ package org.star_pet_house_commons.utils;
 public class VersionCompareUtil {
 
     public static void main(String[] args) {
-        String lastVersion = "V1.0.1";
-        String  version= "V1.0.2";
+        String lastVersion = "V2.1.1";
+        String  version= "V3.1.1";
         if (compareVersion(lastVersion, version)){
             System.out.println("最新版本");
         }else {
-
+            System.out.println("需要更新");
         }
     }
 
@@ -21,12 +21,15 @@ public class VersionCompareUtil {
     public static boolean compareVersion(String oldVersion, String newVersion){
         String[] old_version_strs = oldVersion.replace("V", "").trim().split("\\.");
         String[] new_version_strs = newVersion.replace("V", "").trim().split("\\.");
-        boolean is_new_version = false;
+        boolean is_new_version = true;
         for (int i = 0 ; i < old_version_strs.length; i++){
-            if (Integer.valueOf(old_version_strs[i]) < Integer.valueOf(new_version_strs[i])){
-                is_new_version = true;
-                break;
+            if (Integer.valueOf(old_version_strs[i]) == Integer.valueOf(new_version_strs[i])){
+                continue;
             }
+            if (Integer.valueOf(old_version_strs[i]) > Integer.valueOf(new_version_strs[i])){
+                is_new_version = false;
+            }
+            break;
         }
         return is_new_version;
     }
---
+++ star_pet_house_commons/src/main/java/org/star_pet_house_commons/utiltest/RedisUtilTest.java
@@ -0,0 +1,14 @@
+package org.star_pet_house_commons.utiltest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/*
+ *@description:
+ *@author jiafeng
+ *@date 2020/11/19 0019 11:07
+ */
+public class RedisUtilTest {
+    public static void main(String[] args) {
+        System.out.println("2\"".length());
+    }
+}
